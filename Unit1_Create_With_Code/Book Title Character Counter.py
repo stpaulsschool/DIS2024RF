b6bfe57c68993520757b05c55caef8b0b04aa1af
fickr <20390@stpauls.qld.edu.au>
2023-01-31 15:10:58 +1000

    .

--- Unit1_Create_With_Code/Book Title Character Counter.py
+++ Unit1_Create_With_Code/Book Title Character Counter.py
@@ -1,3 +1,23 @@
-bookTitle = input"What is the title of your book?"
-stringLength = len(bookTitle)
-print("")
+print("Welcome to my little book title counter..." )
+yn = input("Would you like me to count the length of the book's title? ")
+spaceCount = 0
+
+while yn.lower() != "no":
+    if yn.lower() == "yes":
+        bookTitle = input("What is the title of your book? ")
+        for i in range (len(bookTitle)):
+            bookTitleL = bookTitle[i]
+            if bookTitleL == " ":
+                spaceCount = spaceCount+1
+
+
+        stringLength = len(bookTitle)
+        stringLengthWithoutSpaces = stringLength-spaceCount
+        print("Your Book Title " + "'" + bookTitle + "'" + " has " + str(stringLength) + " characters (including spaces), or " + str(stringLengthWithoutSpaces) + " characters (without spaces)... ")
+        spaceCount = 0
+        yn = input("Would you like to try again? ")
+
+    else:
+        print("Sorry I do not understand ")
+
+print("Thank you, goodbye! ")
