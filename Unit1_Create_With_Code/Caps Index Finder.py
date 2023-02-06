5b7baf8c20deebfd0d388135847df8287328f151
fickr <20390@stpauls.qld.edu.au>
2023-02-06 12:23:48 +1000

    .

--- Unit1_Create_With_Code/Caps Index Finder.py
+++ Unit1_Create_With_Code/Caps Index Finder.py
@@ -1,7 +1,10 @@
-word = input("What is your word? ")
-list = []
-for i, v in enumerate(word):
-    if v.isupper():
-        list.append(i)
-print(list)
-
+word = input("What is your word?(type 'q' to quit) ")
+while word.lower() != "q":
+    list = []
+    for i, v in enumerate(word):
+        if v.isupper():
+            list.append(i)
+    print("=" * 50)
+    print("The indexes that were capital letters were: " + str(list))
+    word = input("What is your word?(type 'q' to quit) ")
+print("Thank you, goodbye!")
