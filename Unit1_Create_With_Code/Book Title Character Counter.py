62fff7b259ce91efedacf964857c3d5fada9b63a
fickr <20390@stpauls.qld.edu.au>
2023-02-08 10:14:13 +1000

    .

--- Unit1_Create_With_Code/Book Title Character Counter.py
+++ Unit1_Create_With_Code/Book Title Character Counter.py
@@ -1,6 +1,8 @@
 print("Welcome to my little book title counter..." )
 spaceCount = 0
 bookTitles = []
+lengthList = []
+lengthListNoSpaces = []
 bookTitle = input("What is the title of your book? (Type 'q' to quit) ")
 
 while bookTitle.lower() != "q":
@@ -10,7 +12,9 @@ while bookTitle.lower() != "q":
             if bookTitleL == " ":
                 spaceCount = spaceCount+1
         stringLength = len(bookTitle)
+        lengthList.append(stringLength)
         stringLengthWithoutSpaces = stringLength-spaceCount
+        lengthListNoSpaces.append(stringLengthWithoutSpaces)
         print("Your Book Title " + "'" + bookTitle + "'" + " has " + str(stringLength) + " characters (including spaces), or " + str(stringLengthWithoutSpaces) + " characters (without spaces)... ")
         spaceCount = 0
         bookTitle = input("What is the title of your book? (Type 'q' to quit) ")
@@ -20,5 +24,5 @@ print(bookTitles)
 print(50 * "=")
 print("The books you checked were:")
 for idx, bookTitle in enumerate(bookTitles):
-    print(f"{idx+1}: {bookTitle}")
+    print(f"{idx+1}: {bookTitle} {lengthList[idx]} {lengthListNoSpaces[idx]}")
 print(50 * "=")
--- Unit1_Create_With_Code/Number Guessing Game.py
+++ Unit1_Create_With_Code/Number Guessing Game.py
@@ -1,19 +1,54 @@
 import random
-NOG = 1
+ynList = ["yes", "y", "no", "n"]
+name = input("What is your name? ")
+print("Hello " + name + " and welcome to my number guessing game!")
+HighScore = 6
 
-print("Hello and welcome to my number guessing game!")
-number = random.randint(1, 100)
-print("I have randomly selected a number between 1->100")
-print(number)
+while True:
+    NOG = 1
+    # Number Of Guesses set to 1
+    number = random.randint(1, 100)
+    print("I have randomly selected a number between 1->100...")
 
-while NOG != 7:
-    guess = int(input("guess " + str(NOG) + "/6, please enter your guess... "))
-    if guess == number:
-        print("You guessed it in " + str(NOG) + " guess/es!!!! ")
+
+
+    while NOG != 7:
+        guess = int(input("This is guess " + str(NOG) + "/6, please enter your guess... "))
+        if guess != number and NOG == 6:
+            print("You ran out of guesses...")
+        elif guess == number and NOG == 1:
+            print("WOW you guessed it first try, good job!!!")
+            break
+        elif guess == number:
+            print("You guessed it in " + str(NOG) + " guesses!!!! ")
+            break
+        elif guess>number:
+            print("Lower")
+        elif guess<number:
+            print("Higher")
+        NOG = NOG+1
+
+    if NOG <7:
+        if HighScore>NOG:
+            HighScore = NOG
+        print("HighScore (Number Of Guesses):" + str(HighScore))
+    yn = input("Do you want to play again (type y/n) ")
+    if yn.lower() == "n" or yn.lower() == "no":
+        print("Thank you for playing, goodbye")
         quit()
-    elif guess>number:
-        print("Lower")
-    elif guess<number:
-        print("Higher")
-    NOG = NOG+1
+    elif yn.lower() == "y" or yn.lower() == "yes":
+        print("=" * 50)
+        print("\n")
+    else:
+        while yn.lower() not in ynList:
+            print("Sorry, I do not understand...")
+            yn = input("Do you want to play again (type y/n) ")
+        if yn.lower() == "n" or yn.lower() == "no":
+            print("Thank you for playing, goodbye!")
+            quit()
+        elif yn.lower() == "y" or yn.lower() == "yes":
+            print("=" * 50)
+            print("\n")
+
+
 
