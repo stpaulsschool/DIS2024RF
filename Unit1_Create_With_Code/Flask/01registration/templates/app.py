d9421932fab3ed6b7ce24c3145d139d0c3b0999b
fickr <20390@stpauls.qld.edu.au>
2023-02-17 14:21:23 +1000

    .

--- Unit1_Create_With_Code/Flask/01registration/templates/app.py
+++ Unit1_Create_With_Code/Flask/01registration/templates/app.py
@@ -1,14 +1,26 @@
-#app.py
-
 from flask import Flask, render_template, request
-#from registration import Registration
+from registration import Registration
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'a very long string'
+app.config["SECRET_KEY"] = "utfhukigy789y0uipojuhiopuyg8u9i[ko;lmjilkoh;"
 
 @app.route('/')
 def hello_form():
     return '<H1>Hello, Form!</H1>'
 
+
+@app.route('/registration',methods=['GET','POST'])
+
+def RegisterUser():
+    form = Registration()
+    if request.method == 'POST':
+        if form.validate() == False:
+            return render_template('registration.html',form=form)
+        else:
+            return render_template('success.html')
+    elif request.method == 'GET':
+        return render_template('registration.html',form=form)
+
 if __name__ == '__main__':
     app.run(debug=True)
+
--- Unit1_Create_With_Code/Flask/01registration/templates/registration.py
+++ Unit1_Create_With_Code/Flask/01registration/templates/registration.py
@@ -3,10 +3,10 @@
 from flask_wtf import Form
 from wtforms import StringField, PasswordField, SubmitField
 from wtforms import validators, ValidationError
-from wtforms.validators import InputRequired, Emal, EqualTo
-#from testemail import IsUnique
+from wtforms.validators import InputRequired, Email, EqualTo
+from testemail import IsUnique
 
-class Registraition(Form):
+class Registration(Form):
     email = StringField("email", [validators.InputRequired ("Please enter your email"), validators.Email("invalid email"), IsUnique])
     password = PasswordField('password', [validators.InputRequired ("please enter a password")])
     confirm_password = PasswordField("confirm password", [validators.InputRequired ("password"), validators.EquelTo("password", "passwords must match")])
