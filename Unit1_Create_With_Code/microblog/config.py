81ce4f5dd8d9ae8565e12d5f28d52c0f0f527a85
fickr <20390@stpauls.qld.edu.au>
2023-03-01 12:45:21 +1000

    .

--- Unit1_Create_With_Code/microblog/config.py
+++ Unit1_Create_With_Code/microblog/config.py
@@ -1,6 +1,5 @@
 import os
 
-classs Config(object):
-    Secret_KEY = os.environ.get("SECRET_KEY" or 'a-string-you-will-never-guess'
 
-
+class Config(object):
+    Secret_KEY = os.environ.get("SECRET_KEY" or 'a-string-you-will-never-guess')
--- Unit1_Create_With_Code/microblog/microblog.py
+++ Unit1_Create_With_Code/microblog/microblog.py
@@ -1,4 +1,6 @@
 from app import app
 
+app.config["SECRET_KEY"] = "a-string-you-will-never-guess"
+
 if __name__ == "__main__":
     app.run(debug=True)
