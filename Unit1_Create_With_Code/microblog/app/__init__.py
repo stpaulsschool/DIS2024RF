e5415f8a47112a63bc108066623ad3865d816236
fickr <20390@stpauls.qld.edu.au>
2023-03-03 14:20:13 +1000

    .

--- Unit1_Create_With_Code/microblog/app/__init__.py
+++ Unit1_Create_With_Code/microblog/app/__init__.py
@@ -1,7 +1,13 @@
 from flask import Flask
-from config import Config
+from Unit1_Create_With_Code.microblog.config import Config
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 
 app = Flask(__name__)
 app.config.from_object(Config)
+app.config['SECRET_KEY'] = 'a very long string'
 
-from app import routes
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+
+from app import routes, models
